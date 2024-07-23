9930df53b6dd3d0894858302ad83fdb93561b04b
bruceabrigo <bruceabrigo@outlook.com>
2024-07-23 15:24:02 -0500

    First commit

---
+++ movies/src/main/java/bruceabrigo/dev/movies/Movie.java
@@ -0,0 +1,32 @@
+package bruceabrigo.dev.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    
+    @Id
+
+//    list off properties
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference // manual reference relationship for reviews
+    private List<Review> reviewIds;
+}
---
+++ movies/src/test/java/bruceabrigo/dev/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package bruceabrigo.dev.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
